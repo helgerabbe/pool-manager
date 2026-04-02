af39d8ab7d213aabbff33621655f4cf3a2a00eff
H. Rubble <helgerabbe@googlemail.com>
2026-04-02 22:24:12 +0000

    File changes

--- src/components/basismodule/BasismodulCreateDialog.jsx
+++ src/components/basismodule/BasismodulCreateDialog.jsx
@@ -51,7 +51,6 @@ export default function BasismodulCreateDialog({ open, onOpenChange }) {
     mutationFn: (data) => base44.entities.Basismodule.create(data),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['basismodule'] });
-      queryClient.invalidateQueries({ queryKey: ['basislernpakete'] });
       toast.success('Basismodul erstellt');
       handleReset();
       onOpenChange(false);
