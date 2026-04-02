cdae7432c148079c5f231f902aaa49afdd2d49e9
H. Rubble <helgerabbe@googlemail.com>
2026-04-02 11:43:42 +0000

    File changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -89,7 +89,7 @@ module.exports = {
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out',
-  			'heartbeat': 'heartbeat 3s ease-in-out infinite'
+  			'heartbeat': 'heartbeat 2s ease-in-out infinite'
   		}
   	}
   },
