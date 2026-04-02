c12ef40d56dd093336644e3803f7b51a1ccf2f69
H. Rubble <helgerabbe@googlemail.com>
2026-04-02 13:17:10 +0000

    File changes

--- base44/functions/createEinheitMitDefaults/entry.ts
+++ base44/functions/createEinheitMitDefaults/entry.ts
@@ -35,32 +35,5 @@ Deno.serve(async (req) => {
     sync_status: 'new',
   });
 
-  let themenfeld, lernpaket;
-  try {
-    // Schritt 2: Default-Themenfeld anlegen
-    themenfeld = await base44.entities.Themenfeld.create({
-      einheit_id: einheit.id,
-      titel: 'Themenfeld 1',
-      reihenfolge: 1,
-      sync_status: 'new',
-    });
-
-    // Schritt 3: Default-Lernpaket mit allen Phasen aktiv
-    lernpaket = await base44.entities.Lernpakete.create({
-      einheit_id: einheit.id,
-      themenfeld_id: themenfeld.id,
-      titel_des_pakets: 'Neues Lernpaket',
-      reihenfolge_nummer: 1,
-      geschaetzte_dauer_minuten: 45,
-      phasen_konfiguration: DEFAULT_PHASEN_KONFIGURATION,
-      sync_status: 'new',
-    });
-  } catch (err) {
-    // Rollback
-    if (themenfeld?.id) await base44.entities.Themenfeld.delete(themenfeld.id).catch(() => {});
-    await base44.entities.Einheiten.delete(einheit.id).catch(() => {});
-    return Response.json({ error: `Transaktion fehlgeschlagen: ${err.message}` }, { status: 500 });
-  }
-
-  return Response.json({ einheit, themenfeld, lernpaket });
+  return Response.json({ einheit });
 });
