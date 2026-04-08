c2b22df3f6aac5e2994aa53da55b84a992c7942b
H. Rubble <helgerabbe@googlemail.com>
2026-04-08 14:47:51 +0000

    File changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
+import ErrorBoundary from '@/components/errors/ErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <ErrorBoundary label="der App">
+    <App />
+  </ErrorBoundary>
 )
